33d5cfe2e64adbfe57c848a72a1e1e7d0e919f17
Rusty2549 <arust23@gmail.com>
2026-04-09 16:00:27 -0600

    Day 8 of 100 Days of Code

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,14 @@
+
+# def greet(input):
+#     print(f"Hey! {input}")
+#
+# greet("Tommy")
+
+#Functions with Inputs
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Alex")
+
