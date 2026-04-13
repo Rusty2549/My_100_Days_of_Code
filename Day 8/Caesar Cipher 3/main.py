9313a2a83a3e7b8163277f0fe03f08e78c4aa8af
Rusty2549 <arust23@gmail.com>
2026-04-13 16:07:55 -0600

    Day 8 of 100 Days of Code

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,9 +1,7 @@
-# TODO-1: Import and print the logo from art.py when the program starts.
-
+from art import logo
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-# TODO-2: What happens if the user enters a number/symbol/space?
 
 
 def caesar(original_text, shift_amount, encode_or_decode):
@@ -13,20 +11,23 @@ def caesar(original_text, shift_amount, encode_or_decode):
         if encode_or_decode == "decode":
             shift_amount *= -1
 
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter  #appends char to the end of the output
+
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
+should_continue = True
 
-# TODO-3: Can you figure out a way to restart the cipher program?
-
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
-
-
-
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    restart = input("Do you want to go again? Type yes or no")
+    if restart == "no":
+        should_continue = False
+        print("Goodbye")
