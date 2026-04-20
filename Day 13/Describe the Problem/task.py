c555a83f0ce2a369b3fb5237109f51aabe4c1e14
Rusty2549 <arust23@gmail.com>
2026-04-20 15:31:43 -0600

    Finished Day 13

--- Day 13/Describe the Problem/task.py
+++ Day 13/Describe the Problem/task.py
@@ -8,5 +8,8 @@ my_function()
 
 # Describe the Problem - Write your answers as comments:
 # 1. What is the for loop doing?
+# iterating over a range exclusive of 1 to 20 and then printing out You got it, if i == 20.
 # 2. When is the function meant to print "You got it"?
+# When i is 20.
 # 3. What are your assumptions about the value of i?
+# it can be any value from 1 - 19.
