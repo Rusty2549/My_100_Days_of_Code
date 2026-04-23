b4b1223e80412e0f6b4c78dbb8ddfd2bee342f5d
Rusty2549 <arust23@gmail.com>
2026-04-23 16:32:29 -0600

    Continued Day 15. Just some few touch ups left.

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -29,13 +29,37 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+def gather_coins():
+    print("please insert coins")
+    total = int(input("How many quarters?")) * 0.25
+    total += int(input("How many dimes?")) * 0.10
+    total += int(input("How many nickels?")) * 0.05
+    total += int(input("How many pennies?")) * 0.01
+    return total
+def is_resource_sufficient(chosen_drink, resources):
+    for ingredient in chosen_drink["ingredients"]:
+        if chosen_drink["ingredients"][ingredient] > resources[ingredient]:
+            return False
+    return True
+machine_is_on = True
 chosen_drink = ""
-
-question = input("What would you like? espresso/latte/cappuccino?")
-
-
-if question in MENU:
-    chosen_drink = MENU[question]
-else:
-    print("Sorry that drink is invalid.")
-
+while machine_is_on:
+    drink_choice = input("What would you like? espresso/latte/cappuccino? Or type 'off' to turn off the machine, or "
+                         "'report' to print out the remaining resources.")
+    if drink_choice == "off":
+        machine_is_on = False
+    elif drink_choice == "report":
+        for key, value in resources.items():
+            if key == "coffee":
+                print(f"{key} Remaining: {value}grams")
+            else:
+                print(f"{key} Remaining: {value}ml")
+    elif drink_choice in MENU:
+        chosen_drink = MENU[drink_choice]
+        if is_resource_sufficient(chosen_drink, resources):
+            for ingredient in chosen_drink["ingredients"]:
+                resources[ingredient] -= chosen_drink["ingredients"][ingredient]
+        else:
+            print(f"Sorry not enough resources to make your drink")
+    else:
+        print("Sorry that drink is invalid.")
