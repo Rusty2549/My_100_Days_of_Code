224c6a383a30bb89db13be113f458d610ca15217
Rusty2549 <arust23@gmail.com>
2026-04-29 15:07:02 -0600

    Finished Day 16

--- Day 16-100/oop-coffee-machine-start/main.py
+++ Day 16-100/oop-coffee-machine-start/main.py
@@ -2,22 +2,22 @@ from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-drinks = Menu()
-machine = CoffeeMaker()
-coins = MoneyMachine()
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
 machine_running = True
 
 
 while machine_running:
-    choice = input(f"What would you like? {drinks.get_items()}: ")
+    choice = input(f"What would you like? {menu.get_items()}: ")
     if choice == "off":
         machine_running = False
     elif choice == "report":
-        machine.report()
-        coins.report()
+        coffee_maker.report()
+        money_machine.report()
     else:
-        drink = drinks.find_drink(order_name=choice)
-        if machine.is_resource_sufficient(drink):
-            if coins.make_payment(drink.cost):
-                machine.make_coffee(drink)
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
